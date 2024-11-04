fbe53abb7344ff39c5bd09689d06ce328f17730a
Anonymity Z <zhangyq328@mail2.sysu.edu.cn>
2024-11-04 17:10:22 +0800

    Update VFL.py
    Added a comment

--- VFL.py
+++ VFL.py
@@ -345,4 +345,4 @@ plt.gca().yaxis.set_major_locator(plt.NullLocator())
 plt.subplots_adjust(top=1, bottom=0, left=0, right=1, hspace=0, wspace=0)
 plt.margins(0,0)
 plt.savefig(r'result_outline2\result{}.png'.format(number),dpi=200)
-plt.show()
+plt.show() # The visualization of Figure 8 in the manuscript
